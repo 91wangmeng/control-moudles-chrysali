956f9dbdda11700c047daa8b9c8bf493265f8988
wang_mm <Qq6492178>
2017-08-08 11:01:33 +0800

    1.一点小bug

--- cloud-control-web/src/main/java/com/drore/cloud/control/web/application/TestController.java
+++ cloud-control-web/src/main/java/com/drore/cloud/control/web/application/TestController.java
@@ -3,13 +3,12 @@ package com.drore.cloud.control.web.application;
 import com.drore.cloud.control.manger.common.cache.service.RedisBuilder;
 import com.drore.cloud.control.manger.common.http.utils.HttpRequestUtils;
 import com.drore.cloud.control.manger.common.log.annotations.ServerInvokeLog;
-import com.drore.cloud.control.manger.common.log.enmu.InvokerType;
-import com.drore.cloud.control.manger.common.log.enmu.LogType;
+import com.drore.cloud.control.manger.common.log.constant.InvokerType;
+import com.drore.cloud.control.manger.common.log.constant.LogType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,8 +35,6 @@ public class TestController {
     private String host;
     @Resource
     private RedisBuilder redisBuilder;
-    @Resource
-    private Environment environment;
 
     /**
      * Test string.
@@ -47,7 +44,7 @@ public class TestController {
      * @throws InterruptedException the interrupted exception
      */
     @RequestMapping("/test")
-    @ServerInvokeLog(serverDescription = "管控系统测试接口", logType = LogType.API_LOG_TYPE, invoker = InvokerType.CONTROL_WEB_INVOKEY_TYPE)
+    @ServerInvokeLog(serverDescription = "管控系统测试接口", logType = LogType.API_LOG_TYPE, invoker = {InvokerType.CONTROL_WEB_INVOKER_TYPE, InvokerType.CONTROL_APP_INVOKER_TYPE})
     public String test() throws ExecutionException, InterruptedException {
         redisBuilder.set("a", "b");
         HashMap<String, Object> stringObjectHashMap = new HashMap<>();
